0bc7f885294c392a4ec9b749f8499009eb4c229d
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-10-28 22:33:55 -0700

    vectors

---
+++ ctut_13_vectors.cpp
@@ -0,0 +1,17 @@
+// C++ Programming tutorial video, 1 hour basics
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+	vector <int> lotteryNumVec(10);   //just like arrays, but values can change
+
+	
+
+
+	return 0; 
+}
