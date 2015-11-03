4b5dcd05a64a1e4d5900e61f5d01d8851785304e
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-11-02 16:52:29 -0800

    finish vectors

--- ctut_13_vectors.cpp
+++ ctut_13_vectors.cpp
@@ -10,20 +10,24 @@ int main(){
 
 	vector <int> lotteryNumVect(10);   //just like arrays, but values can change
 
-	int lotteryNumArray[5] = {4, 13, 14, 24, 34};
+	int lotteryNumArray[5] = {4, 13, 14, 24, 34}; // put array inside vector
 
 	lotteryNumVect.insert(lotteryNumVect.begin(), lotteryNumArray,
-	    lotteryNumArray + 3);
+	    lotteryNumArray + 3); // start inserting at start of vector
 
-	lotteryNumVect.insert(lotteryNumVect.begin()+5, 44);
+	lotteryNumVect.insert(lotteryNumVect.begin()+5, 44); 
 
-	cout << lotteryNumVect.at(5) << endl;
+	cout << lotteryNumVect.at(5) << endl; 
 
-	lotteryNumVect.push_back(64);
+	lotteryNumVect.push_back(64); //add value at end of vector
 
-	cout << "Final Value " << lotteryNumVect.back() << endl;
+	cout << "Final Value " << lotteryNumVect.back() << endl; // get final value
+	cout << "First Value " << lotteryNumVect.front() << endl; // first value
+	cout << "Empty " << lotteryNumVect.empty() << endl; // return 0 if not empty
+	// or a 1 if it is empty
+	cout << "Size of " << lotteryNumVect.size() << endl; // get size
 
-	lotteryNumVect.pop_back();
+	lotteryNumVect.pop_back(); 
 
 	return 0; 
 }
