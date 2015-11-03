8d08c7b1c7186c7e79a36003bf9cff42c2d550f5
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-11-02 19:15:15 -0800

    functions inside C==

--- ctut_14_functions.cpp
+++ ctut_14_functions.cpp
@@ -24,11 +24,26 @@ int addNumbers(int firstNum, int secondNum, int thirdNum){
 	return firstNum + secondNum + thirdNum; // can do addition inside return
 }
 
+// recursive functions that reference themselves within themselves
+
+int getFactorial(int number){
+
+	int sum;
+	if(number == 1) sum = 1;
+	else sum = getFactorial(number - 1) * number;
+    
+    return sum;
+
+}
+
+
 int main(){
 
 	cout << addNumbers(1) << endl;
 
 	cout << addNumbers(1, 5, 6) << endl;
+
+	cout << "The factorial of 3 is " << getFactorial(3) << endl;
 
 	return 0; 
 }
