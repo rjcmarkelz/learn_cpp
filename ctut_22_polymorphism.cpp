7b4dade79a62220bfe6b040e2686caad087d7f1b
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-11-05 17:07:21 -0800

    working classes polymorphisms and virtual methods

---
+++ ctut_22_polymorphism.cpp
@@ -0,0 +1,14 @@
+// C++ Programming tutorial video, 1 hour basics
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class 
+
+int main(){
+
+	return 0;
+}
---
+++ ctut_22_virtual_methods.cpp
@@ -0,0 +1,52 @@
+// C++ Programming tutorial video, 1 hour basics
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Plant{
+	public: void getFamily() { cout << "We are plants" << endl; }
+
+	virtual void  getClass() { cout << "I'm a plant" << endl; }
+	// mark this as virtual if method will be overwritten by subclass
+
+};
+
+class Cactus : public Plant{
+public:
+	void getClass() {cout << "I'm a Cactus" << endl; }
+
+};
+
+class Barrel : public Cactus{
+public:
+	void getClass() { cout << "I'm a Barrel Cactus" << endl;}
+	void getDerived() { cout << "I'm a Plant and a Cactus" << endl;}
+}
+
+
+
+
+// call from function
+void whatClassAreYou(Plant *plant){
+
+	plant -> getClass();
+}
+
+int main(){
+
+	Plant *plant = new Plant;
+	Cactus *cactus = new Cactus;
+
+	plant -> getClass();
+	cactus -> getClass();
+
+	whatClassAreYou(plant);
+	whatClassAreYou(cactus);
+
+	
+
+	return 0;
+}
