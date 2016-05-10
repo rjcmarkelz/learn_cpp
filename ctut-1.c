8266155f2d6a7ed901ad081643a479a416ecefef
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-05-10 08:44:32 -0700

    morning c tutorial

---
+++ ctut-1.c
@@ -0,0 +1,29 @@
+/* multiline
+comment */
+
+// single line comment
+
+#include <stdio.h>
+#include <string.h>
+
+#define MYNAME "Cody Markelz" // constant
+
+int globalVar = 100; // camel case
+
+main(){
+
+    char firstLetter = 'D'; // 256 characters
+
+    int age = 31; // -32,768 and 32,768
+
+    long int superBigNum = -32485858; 
+
+    float piValue = 3.14159;
+
+    double reallyBigPi = 3.1415923423423523523523525;
+
+    printf("\n");
+
+    printf("this will print to screen\n");
+
+}
