e28efdf43956c6c956fa7602d274137c3683f75e
Cody Markelz <rjmarkelz@ucdavis.edu>
2017-02-17 17:30:29 -0800

    quick examples to get back into CPP

---
+++ cpp-book-1.cpp
@@ -0,0 +1,11 @@
+// A simple C++ program
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Programming example out";
+    return 0;
+}
---
+++ iostream.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The following items:" << endl;
+    cout << "beer" << endl;
+    cout << "chicken" << endl;
+    cout << "waffles" << endl;
+
+    return 0;
+
+}
